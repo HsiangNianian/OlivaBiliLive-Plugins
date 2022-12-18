1a60b8d94fda5b883455a84efb98106be8566c89
Hsiang Nianian <Hsiangnianian@outlook.com>
2022-12-18 10:14:36 +0800

    时间回复插件

--- time_1.py
+++ time_1.py
@@ -1,10 +1,16 @@
-import OlivaBiliLive
+'''时间回复
+
+简单的交互功能
+是一个插件示例
+'''
+
+import OlivaBiliLive  # 命名空间
 
 from OlivaBiliLive.plugin import BotPlugin, DanmakuMessage
 from datetime import datetime
 
-class TimeChecker(BotPlugin):
-    async def on_command_received(self, cmd, data):
+class TimeChecker(BotPlugin):  # 类名`TimeChecker`将会被作为该插件的命名空间加载，具体显示在日志中。
+    async def on_command_received(self, cmd, data):  # 异步
         if cmd != 'DANMU_MSG':
             return
         danmu = DanmakuMessage.from_command(data['info'])
